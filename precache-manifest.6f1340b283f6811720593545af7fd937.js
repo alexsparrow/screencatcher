a8d5950a47b6d136ca2db3216492b729550bcba5
Alex Sparrow <alex@quartic.io>
2020-06-19 17:38:44 +0100

    Updates

---
+++ precache-manifest.6f1340b283f6811720593545af7fd937.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27e3b0e9ed3f8ff988269dad54a6c099",
+    "url": "/screen2gif/index.html"
+  },
+  {
+    "revision": "6c769d34889ee4ae6bea",
+    "url": "/screen2gif/static/css/2.61537109.chunk.css"
+  },
+  {
+    "revision": "a95ce2f8284c727b7080",
+    "url": "/screen2gif/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6c769d34889ee4ae6bea",
+    "url": "/screen2gif/static/js/2.0d5c0a9e.chunk.js"
+  },
+  {
+    "revision": "e18637604789af6a40503615de5bd745",
+    "url": "/screen2gif/static/js/2.0d5c0a9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a95ce2f8284c727b7080",
+    "url": "/screen2gif/static/js/main.bf84c81f.chunk.js"
+  },
+  {
+    "revision": "1b38110b0d864ea0f81b",
+    "url": "/screen2gif/static/js/runtime-main.e2e4523f.js"
+  },
+  {
+    "revision": "05f1cdadfe476395f60e233b15c22155",
+    "url": "/screen2gif/static/media/icons-16.05f1cdad.eot"
+  },
+  {
+    "revision": "3c1c220e7a18286503fb431c7a7fe183",
+    "url": "/screen2gif/static/media/icons-16.3c1c220e.woff"
+  },
+  {
+    "revision": "3cde8748332d1de6b1ae1c2dc5850754",
+    "url": "/screen2gif/static/media/icons-16.3cde8748.ttf"
+  },
+  {
+    "revision": "0a5c76518a68c185baa2c6744456918c",
+    "url": "/screen2gif/static/media/icons-20.0a5c7651.eot"
+  },
+  {
+    "revision": "51ec31f302d0072808e1f83f85fea4cd",
+    "url": "/screen2gif/static/media/icons-20.51ec31f3.ttf"
+  },
+  {
+    "revision": "cef8cdbb9d0ba82e6e19fb0eeba2ac3d",
+    "url": "/screen2gif/static/media/icons-20.cef8cdbb.woff"
+  }
+]);
