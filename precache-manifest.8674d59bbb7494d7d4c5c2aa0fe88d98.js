fa5c9eeb09eb7f6dcfcdc91214cee324aaa73129
Alex Sparrow <alex@quartic.io>
2020-06-22 14:58:36 +0100

    Updates

---
+++ precache-manifest.8674d59bbb7494d7d4c5c2aa0fe88d98.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2c742808ee4daebb6a575337a632002",
+    "url": "/screencatcher/index.html"
+  },
+  {
+    "revision": "ae5dcf185dbbc45848ee",
+    "url": "/screencatcher/static/css/2.2fa635ab.chunk.css"
+  },
+  {
+    "revision": "1bb615d73a2b18d0bf82",
+    "url": "/screencatcher/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ae5dcf185dbbc45848ee",
+    "url": "/screencatcher/static/js/2.f2faafb9.chunk.js"
+  },
+  {
+    "revision": "0046c4337823700c38288e0c9d577e26",
+    "url": "/screencatcher/static/js/2.f2faafb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bb615d73a2b18d0bf82",
+    "url": "/screencatcher/static/js/main.9ff54626.chunk.js"
+  },
+  {
+    "revision": "1df1720006bbc4427312",
+    "url": "/screencatcher/static/js/runtime-main.91b547df.js"
+  },
+  {
+    "revision": "05f1cdadfe476395f60e233b15c22155",
+    "url": "/screencatcher/static/media/icons-16.05f1cdad.eot"
+  },
+  {
+    "revision": "3c1c220e7a18286503fb431c7a7fe183",
+    "url": "/screencatcher/static/media/icons-16.3c1c220e.woff"
+  },
+  {
+    "revision": "3cde8748332d1de6b1ae1c2dc5850754",
+    "url": "/screencatcher/static/media/icons-16.3cde8748.ttf"
+  },
+  {
+    "revision": "0a5c76518a68c185baa2c6744456918c",
+    "url": "/screencatcher/static/media/icons-20.0a5c7651.eot"
+  },
+  {
+    "revision": "51ec31f302d0072808e1f83f85fea4cd",
+    "url": "/screencatcher/static/media/icons-20.51ec31f3.ttf"
+  },
+  {
+    "revision": "cef8cdbb9d0ba82e6e19fb0eeba2ac3d",
+    "url": "/screencatcher/static/media/icons-20.cef8cdbb.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/screen2gif/precache-manifest.c891dd5ff0ce5b7238287d3d1ff374fc.js"
+  "/screencatcher/precache-manifest.8674d59bbb7494d7d4c5c2aa0fe88d98.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/screen2gif/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/screencatcher/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
